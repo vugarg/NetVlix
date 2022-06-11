3068cf07b1d1ea1b8cbe72eb99990e585e857f52
Vugar Gafarli <vugarg@Vugars-MacBook-Pro.local>
2022-06-11 21:23:59 +0300

    structure

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
 
 const App = () => {
-    <div>App</div>;
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <NavBar />
+            <main className={classes.content}>
+                <div className={classes.toolbar}></div>
+                <Switch>
+                    <Route exact path="/movies/:id">
+                        <MovieInformation />
+                    </Route>
+                    <Route exact path="/actors/:id">
+                        <Actors />
+                    </Route>
+                    <Route exact path="/">
+                        <Movies />
+                    </Route>
+                    <Route exact path="/profiles/:id">
+                        <Profile />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 };
 
 export default App;
