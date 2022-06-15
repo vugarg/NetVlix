43ad683927554618c1fdec82c299b9bbe40f4afb
Vugar Gafarli <vugarg@Vugars-MacBook-Pro.local>
2022-06-15 21:25:55 +0300

    added sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
 import {
     AppBar,
@@ -12,8 +12,10 @@ import {
 import { Menu, AccountCircle, LightMode, DarkMode } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
+import { Sidebar } from '..';
 
 const NavBar = () => {
+    const [mobileOpen, setMobileOpen] = useState(false);
     const classes = useStyles();
     const theme = useTheme();
     const isMobile = useMediaQuery('(max-width:600px)');
@@ -22,13 +24,17 @@ const NavBar = () => {
     return (
         <>
             <AppBar position="fixed">
-                <Toolbar className={classes.toolbar} color="orange">
+                <Toolbar className={classes.toolbar}>
                     {isMobile && (
                         <IconButton
                             color="inherit"
                             edge="start"
                             style={{ outline: 'none' }}
-                            onClick={() => {}}
+                            onClick={() =>
+                                setMobileOpen(
+                                    (prevMobileOpen) => !prevMobileOpen
+                                )
+                            }
                             className={classes.menuButton}
                         >
                             <Menu />
@@ -39,7 +45,7 @@ const NavBar = () => {
                         sx={{ ml: 1 }}
                         onClick={() => {}}
                     >
-                        {theme.palette.mode === 'dark' ? (
+                        {theme.palette.mode === 'light' ? (
                             <LightMode />
                         ) : (
                             <DarkMode />
@@ -71,6 +77,34 @@ const NavBar = () => {
                     {isMobile && 'Search...'}
                 </Toolbar>
             </AppBar>
+            <div>
+                <nav className={classes.drawer}>
+                    {isMobile ? (
+                        <Drawer
+                            variant="temporary"
+                            anchor="right"
+                            open={mobileOpen}
+                            onClose={() =>
+                                setMobileOpen(
+                                    (prevMobileOpen) => !prevMobileOpen
+                                )
+                            }
+                            classes={{ paper: classes.drawerPaper }}
+                            ModalProps={{ keepMounted: true }}
+                        >
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    ) : (
+                        <Drawer
+                            classes={{ paper: classes.drawerPaper }}
+                            variant="permanent"
+                            open
+                        >
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    )}
+                </nav>
+            </div>
         </>
     );
 };
